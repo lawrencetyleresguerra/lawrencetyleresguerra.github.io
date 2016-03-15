8e4969dad1dcc297959d50fd8bd16b2c77aa1c52
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 11:09:48 -0400

    Clients.js reset
    Reset to alert

--- myClonedFolder/otherProjects/ajaxExamples/js/clients.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/clients.js
@@ -1,9 +1,1 @@
-$.get("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/clients.html", function(data){
-    
-    
-$(document).ready(function(){
-
-    $(".container").prepend(data);
-    
-    });
-});
+alert("It works!")
