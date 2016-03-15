67da9bb3993cef9c1719926f2542f574c4efdf6b
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 10:07:12 -0400

    Style.css added
    Style.css added

--- myClonedFolder/js/getnav.js
+++ myClonedFolder/js/getnav.js
@@ -4,6 +4,7 @@ $.get("http://lawrencetyleresguerra.github.io/myClonedFolder/partials/nav.html",
 $(document).ready(function(){
 
     $(".container").prepend(data);
+    $(".container").fadeIn();
     
     });
 });
