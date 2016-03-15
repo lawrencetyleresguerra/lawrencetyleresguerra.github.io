0284860c37d4c4d18d9dc84a998e5c28db47201c
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 12:04:17 -0400

    Table js
    title

--- myClonedFolder/otherProjects/ajaxExamples/js/clients.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/clients.js
@@ -3,14 +3,23 @@ $(document).ready(function() {
     $("#getClients").on("click", function() {
         var url = "http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json";
         $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
+            var html = "<table>"+
+        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+            
             $.each(data, function(index, item){
-                $("#data").append(item.name);
+        html+="<tr>"
+            "<td>" + item.name + "</td>" +
+            "<td>" + item.email + "</td>" +
+            "<td>" + item.company + "</td>" +
+            "</tr>";
             })
+            
+            html += "</table>";
+            $("#data").append(html);
 
             //alert(data);
             //console.dir(data);
 
-        });
-
-    });
-});
+        });//getJSON
+    });//click
+});//function
