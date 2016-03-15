4c3262afede2e030fa201aa367e69a988db1a4fc
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 11:30:47 -0400

    Alert data on json
    Title

--- myClonedFolder/otherProjects/ajaxExamples/js/clients.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/clients.js
@@ -1,5 +1,9 @@
 $("#getClients").on("click", function(){
-    $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json");
+    $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data){
+        
+        alert(data);
+        console.dir(data);
+    });
     
 })
 
