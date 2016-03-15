3c95e98d0127393332a48a1aec8986f2a5ebf725
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 11:40:01 -0400

    Json changed
    title

--- myClonedFolder/otherProjects/ajaxExamples/js/clients.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/clients.js
@@ -1,10 +1,13 @@
-$("#getClients").on("click", function(){
-    $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data){
-        
-        alert(data);
-        console.dir(data);
-    
-    });
-    
-})
+$(document).ready(function() {
 
+    $("#getClients").on("click", function() {
+        var url = "http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json";
+        $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
+
+            alert(data);
+            console.dir(data);
+
+        });
+
+    });
+});
