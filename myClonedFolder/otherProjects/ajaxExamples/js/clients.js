8c295a3a7046b1b5936cc50aa831cb459d74ba1b
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 11:27:54 -0400

    Linked to Clients.json
    title

--- myClonedFolder/otherProjects/ajaxExamples/js/clients.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/clients.js
@@ -1,5 +1,5 @@
 $("#getClients").on("click", function(){
+    $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json");
     
-    alert("It works!")
 })
 
