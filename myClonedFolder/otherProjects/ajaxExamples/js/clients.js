7a6b0bf32e4a0fb027bf55ecc2d1280399ec9e9e
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 12:07:41 -0400

    table making js
    title

--- myClonedFolder/otherProjects/ajaxExamples/js/clients.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/clients.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     $("#getClients").on("click", function() {
         var url = "http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json";
         $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
-            var html = "<table>"+
+            var html = "<table class='table table-hover table-striped'>"+
         "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
             
             $.each(data, function(index, item){
-        html+="<tr>"
+                
+        html += "<tr>" +
             "<td>" + item.name + "</td>" +
             "<td>" + item.email + "</td>" +
             "<td>" + item.company + "</td>" +
