eba90fad71f9fa18a593ba0303c96d6655d61908
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-15 10:40:25 -0400

    Footer stuff
    Added footer stuff

---
+++ myClonedFolder/js/getFooter.js
@@ -0,0 +1,9 @@
+$.get("http://lawrencetyleresguerra.github.io/myClonedFolder/partials/footer.html", function(data){
+    
+    
+$(document).ready(function(){
+
+    $(".container").append(data);
+    
+    });
+});
