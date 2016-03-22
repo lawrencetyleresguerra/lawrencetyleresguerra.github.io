e6a3292e2ceccb78253073c6bafe1642a7440d19
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-22 09:52:05 -0400

    JSON updated
    title

--- myClonedFolder/otherProjects/ajaxExamples/js/Jsonmill.js
+++ myClonedFolder/otherProjects/ajaxExamples/js/Jsonmill.js
@@ -1,6 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/Jsonmill.json", function(data){
-      console.dir(data);
-  })
+    $.getJSON("http://lawrencetyleresguerra.github.io/myClonedFolder/otherProjects/ajaxExamples/jsonDatabase/Jsonmill.json", function(data) {
+        console.dir(data);
+        var html = "";
+
+        $.each(data, function(index, item) {
+            html += '<div class="col-md-4" clothing>' +
+                '<div class="clothingType">' + item.type + '</div>' +
+                '<div class="clothingName">' + item.name + '</div>' +
+                '<div class="clothingColor">' + item.color + '</div>' +
+                '<img src=""' + item.image + '/>';
+
+            html += '</div>';
+
+        }) //each cat
+        $("#clothingData").append(html);
+    })
 })
+
+/*
+//one per cat
+<div class="col-md-4">
+    <div class="clothingType"></div>
+    <div class="clothingName"></div>
+    <div class="clothingColor"></div>
+    <img src=""/>
+    <div class="commentsContainer">
+        //one per comment
+        <div class="buyerName"></div>
+        <div class="buyerLocation"></div>
+        <div class="buyerStars">
+        //5 stars, some full, some empty
+        </div> //end stars
+    </div> //end commentsContainer
+</div> //end clothing
+*/
