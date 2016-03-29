6b977e6ba85735b3e855f7f5938c929f2690221c
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-29 08:58:55 -0400

    Final JS and Index updated
    Title.

--- myClonedFolder/otherProjects/FinalProject/js/final.js
+++ myClonedFolder/otherProjects/FinalProject/js/final.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
     //get all the nav li, add click event
-    $(".nav").find("li").on("click", function(){
-    //reomve all active class
-    $(".nav").find("li").removeClass("active");
-    //add active class to clicked li
-    $(this).addClass("active");
-    
-    var page = $(this).attr("id");
-    alert(page);
+    $(".nav").find("li").on("click", function() {
+        //reomve all active class
+        $(".nav").find("li").removeClass("active");
+        //add active class to clicked li
+        $(this).addClass("active");
+
+        var page = $(this).attr("id");
+        alert(page);
     })
 })
