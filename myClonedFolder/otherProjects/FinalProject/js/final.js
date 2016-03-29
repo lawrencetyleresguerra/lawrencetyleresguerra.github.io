e216acc18411c9e7d87f5b235f28cb4b5a8f0efb
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-29 08:54:47 -0400

    Trying to make the Final Index work.
    Title.

--- myClonedFolder/otherProjects/FinalProject/js/final.js
+++ myClonedFolder/otherProjects/FinalProject/js/final.js
@@ -1,1 +1,9 @@
-/* */
+$(document).ready(function(){
+    //get all the nav li, add click event
+    $(".nav").find("li").on("click", function(){
+    //reomve all active class
+    $(".nav").find("li").removeClass("active");
+    //add active class to clicked li
+    $(this).addClass("active");
+    })
+})
