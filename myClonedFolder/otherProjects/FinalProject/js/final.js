ffbb1d15a194573c34ea3d5150e72ed01f112e3d
Lawrence Tyler Esguerra <lawrencetyleresguerra@gmail.com>
2016-03-29 08:55:36 -0400

    Staying caught up with JS Final.
    Title.

--- myClonedFolder/otherProjects/FinalProject/js/final.js
+++ myClonedFolder/otherProjects/FinalProject/js/final.js
@@ -5,5 +5,8 @@ $(document).ready(function(){
     $(".nav").find("li").removeClass("active");
     //add active class to clicked li
     $(this).addClass("active");
+    
+    var page = $(this).attr("id");
+    alert(page);
     })
 })
